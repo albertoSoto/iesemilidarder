77b8e8cbb009e8c74ce76fcad58d27bd18ab0031
Alberto Soto Fernández <alberto.soto@gmail.com>
2018-01-09 17:32:18 +0100

    asd

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # iesemilidarder
+
+Changes:
+
+- Interfaces
+- Abstract classes
+- Conventional classes
+- Inheritance
+- Collections
+- Logging
+- Maven
+- Multimodule aproach
+- Microframework with Spark
+- Debug
--- dummyweb/src/main/java/com/iesemilidarder/fordummies/web/Launcher.java
+++ dummyweb/src/main/java/com/iesemilidarder/fordummies/web/Launcher.java
@@ -1,6 +1,9 @@
 package com.iesemilidarder.fordummies.web;
 
+import com.iesemilidarder.fordummies.core.DBObject;
 import com.iesemilidarder.fordummies.core.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +23,7 @@ import static spark.Spark.port;
  */
 public class Launcher {
     private static List<User> lUser = new ArrayList<User>();
+    private static final Logger log = LoggerFactory.getLogger(Launcher.class);
 
     private static void init() {
         for (int i=0;i<10;i++){
@@ -29,6 +33,8 @@ public class Launcher {
             user.save();
             lUser.add(user);
         }
+        DBObject dbo = lUser.get(0);
+        log.info("Loading finished");
     }
 
     public static void main(String... args) {
